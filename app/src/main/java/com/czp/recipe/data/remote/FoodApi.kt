46538d91e9ab68e6ea727598ed65bba14afde0ca
likepoem-oops <2605597055@qq.com>
2023-08-20 16:28:46 +0800

    修改url参数number=10 -> number=50

--- app/src/main/java/com/czp/recipe/data/remote/FoodApi.kt
+++ app/src/main/java/com/czp/recipe/data/remote/FoodApi.kt
@@ -8,7 +8,7 @@ import retrofit2.http.Query
 interface FoodApi {
     // baseUrl: https://api.spoonacular.com/
     // 获取所有数据
-    @GET("recipes/complexSearch?apiKey=a00ff51ceb9f4b15959db96db2429eaf&addRecipeInformation=true&number=10&cuisine=Chinese&fillIngredients=true")
+    @GET("recipes/complexSearch?apiKey=a00ff51ceb9f4b15959db96db2429eaf&addRecipeInformation=true&number=50&cuisine=Chinese&fillIngredients=true")
     suspend fun fetchFoodRecipes(@Query(UrlString.TYPE) type: String): Response<FoodRecipeBaseInfo>
 
 
